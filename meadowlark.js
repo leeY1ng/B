0c5e5a51d1d42d7e7c2f0d1fc71795512e078d87
leeY1ng <869500351@qq.com>
2018-01-23 18:01:27 +0800

    incorrect some mistakes

--- meadowlark.js
+++ meadowlark.js
@@ -13,17 +13,22 @@ app.set("port", process.env.PORT || 3000);
 
 app.use(express.static(__dirname+"/public"));
 
+app.use(function(req,res,next){
+	res.locals.showTests = app.get("env") !== "production" && req.query.test === "1";
+	next();
+});
+
 app.get("/",function(req,res){
 	res.render("home");
-})	
+});
 app.get("/about",function(req,res){
-	res.render("about",{fortune:fortune.getFortune()})
-})
+	res.render("about",{fortune:fortune.getFortune()});
+});
 //make 404 page
-app.use(function(req,res){	
+app.use(function(req,res,next){	
 	res.status(404);
 	res.render("404");
-})
+});
 //make 500 page 
 app.use(function(err,req,res,next){
 	console.error(err.stack);
@@ -33,4 +38,4 @@ app.use(function(err,req,res,next){
 
 app.listen(app.get("port"),function(){
 	console.log("Express started on http://localhost" +  app.get("port") + ";press Ctrl+c to ternimate");
-})
+});
